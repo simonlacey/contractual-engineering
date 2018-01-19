ff151b1b92e36f655bdfc64fbd4762dced33d5cf
simonlacey <slacey@sileads.com>
2018-01-18 19:58:42 -0500

    updates

---
+++ src/pages/form-to-email.php
@@ -0,0 +1,7 @@
+<div class="grid-x grid-padding-x" style="height:75vh;">
+    <div class="small-12 cell text-center">
+        <h1>Thank you for submitting your info</h1>
+        <h3>One of our licensed professionals with review your submission and get back in touch with you soon.<br>
+        Have a great day.</h3>
+    </div>
+</div>
---
+++ src/pages/thankyou.php
@@ -0,0 +1,7 @@
+<div class="grid-container">
+    <div class="grid-x grid-padding-x">
+        <div class="small-12 cell">
+            <h2>Thank you</h2>
+        </div>
+    </div>
+</div>
